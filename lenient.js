084d572337a36149dea991f134273f46d5aba77f
Joey Cozza <joeycozza@gmail.com>
2019-03-06 14:00:27 -0700

    adding a file for lenient rules. everything is a warning instead of an error

---
+++ lenient.js
@@ -0,0 +1,6 @@
+//This file basically turns everything into warnings instead of errors, 
+//that way during development, create-react-apps wont freak out and break the build
+module.exports = {
+  plugins: ['only-warn'],
+  extends: ['@fs/eslint-config-frontier-react'],
+}
