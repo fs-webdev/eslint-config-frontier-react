3b789f3b17c21ff4c0f6a0ec503a13ba772b7504
Joey Cozza <joeycozza@gmail.com>
2019-03-12 16:16:44 -0600

    turning off jsx extension rule

--- react.js
+++ react.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
