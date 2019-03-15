8daff26abea1aa83d5eac3cbfa6b6a52f00a58c2
Joey Cozza <joeycozza@gmail.com>
2019-03-15 16:53:02 -0600

    turn off react/require-default-props

--- react.js
+++ react.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     'react/prop-types': 'off',
     'react/jsx-filename-extension': 'off',
+    'react/require-default-props': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
