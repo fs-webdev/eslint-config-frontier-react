037a1a0036630abc5fa675dedad8ef2f9a428eda
Joey Cozza <joeycozza@gmail.com>
2019-06-14 14:01:03 -0600

    more changes

--- codeclimateEslintRules.js
+++ codeclimateEslintRules.js
@@ -1,66 +1,83 @@
 module.exports = {
-  plugins: ['html', 'json', 'babel', 'react-hooks'],
-  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react', 'plugin:jest/recommended'],
-  rules: {
-    'prettier/prettier': [
-      'warn',
+  "plugins": [
+    "html",
+    "json",
+    "babel",
+    "react-hooks"
+  ],
+  "extends": [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "plugin:jest/recommended"
+  ],
+  "rules": {
+    "prettier/prettier": [
+      "warn",
       {
-        printWidth: 100,
-        singleQuote: true,
-        trailingComma: 'es5',
-        semi: false,
-      },
+        "printWidth": 100,
+        "singleQuote": true,
+        "trailingComma": "es5",
+        "semi": false
+      }
     ],
-    'arrow-parens': [1, 'as-needed'],
-    'no-console': 'off',
-    'no-param-reassign': 'off',
-    'no-plusplus': 'off',
-    'no-unused-expressions': [
-      'warn',
+    "arrow-parens": [
+      1,
+      "as-needed"
+    ],
+    "no-console": "off",
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-unused-expressions": [
+      "warn",
       {
-        allowShortCircuit: true,
-      },
+        "allowShortCircuit": true
+      }
     ],
-    'no-use-before-define': [
-      'error',
+    "no-use-before-define": [
+      "error",
       {
-        functions: false,
-        classes: true,
-        variables: true,
-      },
+        "functions": false,
+        "classes": true,
+        "variables": true
+      }
     ],
-    'no-warning-comments': 1,
-    'prefer-destructuring': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
+    "no-warning-comments": 1,
+    "prefer-destructuring": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
       {
-        devDependencies: ['**/*.stories.js', '**/*.test.js', '**/demo/**'],
-      },
+        "devDependencies": [
+          "**/*.stories.js",
+          "**/*.test.js",
+          "**/demo/**"
+        ]
+      }
     ],
-    'jsx-a11y/label-has-for': 0,
-    'jsx-a11y/label-has-associated-control': [
+    "jsx-a11y/label-has-for": 0,
+    "jsx-a11y/label-has-associated-control": [
       2,
       {
-        assert: 'either',
-      },
+        "assert": "either"
+      }
     ],
-    'react/prop-types': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/require-default-props': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jest/no-large-snapshots': 'error',
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": "off",
+    "react/require-default-props": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "jest/no-large-snapshots": "error"
   },
-  env: {
-    es6: true,
-    browser: true,
+  "env": {
+    "es6": true,
+    "browser": true
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
-  parser: 'babel-eslint',
+  "parser": "babel-eslint"
 }
