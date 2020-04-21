598cb437f01722014952edcf24d0be2d9bd589f7
Joey Cozza <joeycozza@gmail.com>
2020-04-21 14:18:25 -0600

    dont need lodash with certain rules turned off

--- dont-need-lodash.js
+++ dont-need-lodash.js
@@ -1,4 +1,17 @@
 module.exports = {
-  extends: ['plugin:you-dont-need-lodash-underscore/compatible-warn'],
-  rules: {},
+  extends: ['plugin:you-dont-need-lodash-underscore/compatible'],
+  rules: {
+    'you-dont-need-lodash-underscore/chunk': 'off',
+    'you-dont-need-lodash-underscore/difference': 'off',
+    'you-dont-need-lodash-underscore/fromPairs': 'off',
+    'you-dont-need-lodash-underscore/intersection': 'off',
+    'you-dont-need-lodash-underscore/groupBy': 'off',
+    'you-dont-need-lodash-underscore/minBy': 'off',
+    'you-dont-need-lodash-underscore/maxBy': 'off',
+    'you-dont-need-lodash-underscore/sortBy': 'off',
+    'you-dont-need-lodash-underscore/debounce': 'off',
+    'you-dont-need-lodash-underscore/throttle': 'off',
+    'you-dont-need-lodash-underscore/extend': 'off',
+    'you-dont-need-lodash-underscore/pickBy': 'off',
+  },
 }
