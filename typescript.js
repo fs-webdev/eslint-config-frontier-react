69adf849a0ac15d835962dd11853c78e9a194c96
Joey Cozza <joeycozza@gmail.com>
2020-06-17 10:11:56 -0600

    tweaking a few typescript rules. making v7 release

--- typescript.js
+++ typescript.js
@@ -7,6 +7,11 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
       extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+        '@typescript-eslint/no-unused-var': ['error', { argsIgnorePattern: '^_' }],
+      },
     },
   ],
 }
