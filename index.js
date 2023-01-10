dfa2096ec2856fa0466222f24178240c0a743daf
Jordi Kloosterboer <vetaz@familysearch.org>
2023-01-10 15:53:13 -0700

    update to latests

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  plugins: ['html', 'json'],
+  plugins: ['json'],
   extends: [
-    '@fs/eslint-config-frontier-react/es6',
     '@fs/eslint-config-frontier-react/react',
+    '@fs/eslint-config-frontier-react/es6',
+    '@fs/eslint-config-frontier-react/typescript',
     '@fs/eslint-config-frontier-react/jest',
-    '@fs/eslint-config-frontier-react/test-helpers',
     // '@fs/eslint-config-frontier-react/noFixRules',
     '@fs/eslint-config-frontier-react/dont-need-lodash',
-    '@fs/eslint-config-frontier-react/prettierSetup',
+    '@fs/eslint-config-frontier-react/prettierSetup', // Always have prettier last so it can override format rules in the extends before it
   ],
 }
--- react.js
+++ react.js
@@ -1,12 +1,15 @@
 module.exports = {
   plugins: ['react-hooks'],
-  extends: ['prettier/react'],
+  extends: ['airbnb', 'prettier'], // Always have prettier last so it can override format rules in the extends before it'prettier'],
   env: {
     browser: true,
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
     },
   },
   rules: {
@@ -16,6 +19,16 @@ module.exports = {
     'react/no-unknown-property': ['error', { ignore: ['css', 'columns'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    // 'react/jsx-newline': 'off',
+    'react/function-component-definition': 'off', // TODO look into this more
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+
+    // Overrides airbnb's rule
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either', // fs-styles does not support nesting currently
+      },
+    ],
   },
 }
