d6868f89862e6f7f2bcee38c30338f031acb7a88
Jordi Kloosterboer <vetaz@familysearch.org>
2023-03-14 11:46:36 -0600

    Put prettier config in own file

--- prettierSetup.js
+++ prettierSetup.js
@@ -1,16 +1,10 @@
 // this is following the pattern defined here
 // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+const prettierConfig = require('./prettierConfig')
+
 module.exports = {
   extends: ['plugin:prettier/recommended'],
   rules: {
-    'prettier/prettier': [
-      'warn',
-      {
-        arrowParens: 'always',
-        printWidth: 120,
-        semi: false,
-        singleQuote: true,
-      },
-    ],
+    'prettier/prettier': ['warn', prettierConfig],
   },
 }
