ae23975c8cddf6a1539c325195677bba9f1d8d50
Tyler Graf <tylergraf@gmail.com>
2023-04-20 16:22:25 -0600

    bump version

--- cypress.js
+++ cypress.js
@@ -1,22 +1,21 @@
 module.exports = {
   extends: ['plugin:cypress/recommended'],
-  plugins: ['cypress', '@fs/zion'],
   rules: {
-    'jest/no-large-snapshots': 'error',
-    '@fs/zion/prefer-zion-render': 'off',
-    'testing-library/no-debugging-utils': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.cy.{js,ts,tsx}'],
+      },
+    ],
   },
   overrides: [
     {
       files: ['**/*.spec.*', '**/*.cy.*'],
       rules: {
-        'testing-library/await-async-query': 'off',
-        'testing-library/no-await-sync-query': 'error',
-        'testing-library/no-debug': 'warn',
         'jest/expect-expect': 'off',
         'jest/valid-expect-in-promise': 'off',
         'jest/valid-expect': 'off',
-        'babel/no-unused-expressions': 'off',
+        '@babel/no-unused-expressions': 'off',
       },
     },
   ],
