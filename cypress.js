5aeaa3183f02f6212289e9245332aac1e5b9e691
Tyler Graf <tylergraf@gmail.com>
2024-08-15 19:45:49 -0600

    update cypress config

--- cypress.js
+++ cypress.js
@@ -1,13 +1,5 @@
 module.exports = {
   extends: ['plugin:cypress/recommended'],
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['**/*.{spec,test,cy}.{js,ts,tsx}', 'cypress.config.js'],
-      },
-    ],
-  },
   overrides: [
     {
       files: ['**/*.spec.*', '**/*.cy.*'],
@@ -16,8 +8,7 @@ module.exports = {
         'jest/valid-expect-in-promise': 'off',
         'jest/valid-expect': 'off',
         '@babel/no-unused-expressions': 'off',
-        'testing-library/await-async-utils': 'off',
-        'testing-library/prefer-screen-queries': 'off',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.cy.*', '**/*.spec.*'] }],
       },
     },
   ],
