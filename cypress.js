b920a6d3dd0b48ad9be4661efd6dbed625bd0f31
Jordi Kloosterboer <vetaz@familysearch.org>
2025-09-10 15:32:26 -0600

    Add cypress mocha linting rules

--- cypress.js
+++ cypress.js
@@ -5,11 +5,18 @@ module.exports = {
       files: ['**/*.spec.*', '**/*.cy.*'],
       plugins: ['mocha'],
       rules: {
-        'jest/expect-expect': 'off',
-        'jest/valid-expect-in-promise': 'off',
-        'jest/valid-expect': 'off',
-        '@babel/no-unused-expressions': 'off',
-        'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.cy.*', '**/*.spec.*'] }],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        '@babel/no-unused-expressions': 'off', // Disabled to allow chai expect style assertions.
+        'mocha/consistent-spacing-between-blocks': 'error', // Good for readability and consistence.
+        'mocha/no-async-describe': 'error', // Helpful to make sure describes are not async.
+        'mocha/no-empty-description': 'error', // Make sure there are titles for suites/tests.
+        'mocha/no-exports': 'error', // Shouldn't export from a test file, use a utility file instead if needed to export something.
+        'mocha/no-identical-title': 'error', // This is an easy way to see if you just copy and pasted a test and didn't change the title.
+        'mocha/no-nested-tests': 'error', // This is a developer error and the nested tests will be ignored.
+        'mocha/no-return-and-callback': 'error', // Mocha v3 errors on this as it is confusing to do.
+        'mocha/no-sibling-hooks': 'error', // It is confusing to have multiple before or other hooks on the same level.
+        'mocha/no-exclusive-tests': 'error', // Reminds to remove .only tests or to give a reason why they are skipped.
+        'mocha/no-skipped-tests': 'error', // Reminds to remove .skip tests or to give a reason why they are skipped.
       },
     },
     {
