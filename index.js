e1a9f5eabfadba0ae9031cf68a655b8e0d6900f6
Jordi Kloosterboer <vetaz@familysearch.org>
2026-03-17 11:00:00 -0600

    Include typescript config in default export

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
     '@fs/eslint-config-frontier-react/jest',
     '@fs/eslint-config-frontier-react/cypress',
     '@fs/eslint-config-frontier-react/dont-need-lodash',
+    '@fs/eslint-config-frontier-react/typescript',
     '@fs/eslint-config-frontier-react/prettierSetup', // Always have prettier last so it can override format rules in the extends before it
   ],
   overrides: [{ files: ['src/**'], extends: ['@fs/eslint-config-frontier-react/esx'] }],
